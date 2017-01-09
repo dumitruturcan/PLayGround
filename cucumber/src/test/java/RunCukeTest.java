02325a5d00d547bcaefe17f817270e2cfb7b5a0f
Dumitru Turcan <dumitru.turcan@email.com>
2017-01-10 00:53:07 +0200

    Added dataStructures module

---
+++ cucumber/src/test/java/RunCukeTest.java
@@ -0,0 +1,7 @@
+import cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(tags = "@Run")
+public class RunCukeTest {
+}
---
+++ cucumber/src/test/resources/cucumber.feature
@@ -0,0 +1,6 @@
+Feature: First Cucumber Scenario
+  Scenario: Hop
+    Given there is logged user
+    And bla bla bla
+    When Bla bla bla
+    Then bla bla bla
